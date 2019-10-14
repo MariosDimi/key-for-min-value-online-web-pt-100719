d6fe1aaef76be0c9c51f18eeaf288eded50f0269
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-14 02:40:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash = {:computers => 20, :phones => 35, :tablets => 40}
+  key_for_min_value(name_hash)
 end
