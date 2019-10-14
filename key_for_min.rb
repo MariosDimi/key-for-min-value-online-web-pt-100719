90a8e1d0ba2187ade1109d7ba8648ae484d2fb45
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-14 02:41:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,9 @@
 def key_for_min_value(name_hash)
  lowest_value = 0
   name_hash.each do |key,value|
-    
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+    end
+  end
+  lowest_key
 end
